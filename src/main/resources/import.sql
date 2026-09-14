89c8ce64fbb90ddd0a77bbb68e5b2cc8b7342b1c
alessio falasca <Al3sSi0>
2026-09-14 13:55:01 +0200

    update

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 INSERT INTO utente (id, username, password, ruolo) VALUES (nextval('utente_seq'), 'admin', '{noop}admin', 'ADMIN');
-INSERT INTO regista (id, nome, cognome) VALUES (nextval('regista_seq'), 'Christopher', 'Nolan');
+INSERT INTO regista (id, nome, cognome, data_nascita, nazionalita) VALUES (nextval('regista_seq'), 'Christopher', 'Nolan', '1970-07-30', 'Britannica');
 
 INSERT INTO film (id, titolo, anno, durata, genere, paese_produzione, regista_id, url_immagine) VALUES (nextval('film_seq'), 'Inception', 2010, 148, 'Fantascienza', 'USA', currval('regista_seq'), 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsXv8nE0Zvz6zOA3u-qMAUXVp40_C-BqhCSJTxijdufQ&s=10');
 
@@ -9,7 +9,7 @@ INSERT INTO sala (id, nome, indirizzo, capienza) VALUES (nextval('sala_seq'), 'S
 
 INSERT INTO utente (id, username, password, ruolo) VALUES (nextval('utente_seq'), 'mario', '{noop}user', 'USER');
 
-INSERT INTO regista (id, nome, cognome) VALUES (nextval('regista_seq'), 'Leitch', 'David');
+INSERT INTO regista (id, nome, cognome, data_nascita, nazionalita) VALUES (nextval('regista_seq'), 'Leitch', 'David', '1975-11-16', 'Americana');
 
 INSERT INTO film (id, titolo, anno, durata, genere, paese_produzione, regista_id, url_immagine) VALUES (nextval('film_seq'), 'Deadpool 2', 2018, 119, 'Supereroi', 'USA', currval('regista_seq'), 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSz5kvR1P6vHKsCCfmhwpR3cfTGo9OqRrmqojkxOsQWYw&s=10');
 
